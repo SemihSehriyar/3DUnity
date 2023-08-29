69d58e750389dae677a234e88a795fc13e52f85a
Selim ŞEHRİYAROĞLU <semsehriyar@gmail.com>
2023-08-29 17:34:48 +0300

    Player components added 2
    Player components added 2

--- 3DUnityProject/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
+++ 3DUnityProject/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-   
+
 public class PlayerController : MonoBehaviour
 {
+	private Rigidbody _rigidbody;
 
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		//input
+	}
+
+	private void FixedUpdate()
+	{
+		// fizik islemleri		
+	}
 }
